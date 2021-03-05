12a35c4288237d41c4c5d48f6d63ebf80e52671e
halim-boussada <halimboussada10@gmail.com>
2021-03-05 22:23:37 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test
+In this hard assignment you will need to console log the number 2 
---
+++ test.js
@@ -0,0 +1,6 @@
+function returntwo() {
+  // make this function console log  2 as a number
+  // your code here <3
+}
+
+returntwo();
